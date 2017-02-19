e534b0abaa0cb4fbe1d1a36909a3ea87dbbff205
ujwalp15 <ujwalp09@gmail.com>
2017-02-19 13:03:59 +0530

    simpleai: link github accounts to AboutActivity

--- app/src/main/java/com/example/android/Bella/AboutActivity.java
+++ app/src/main/java/com/example/android/Bella/AboutActivity.java
@@ -1,17 +1,61 @@
 package com.example.android.Bella;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by Ashish Nayak on 06-Feb-17.
- */
+import android.view.View;
+import android.widget.ImageView;
 
 public class AboutActivity extends AppCompatActivity {
+
+    ImageView levi;
+    ImageView astrix;
+    ImageView den;
+    ImageView raiden;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+
+        levi = (ImageView) findViewById(R.id.imageView3);
+        astrix = (ImageView) findViewById(R.id.imageView5);
+        den = (ImageView) findViewById(R.id.imageView7);
+        raiden = (ImageView) findViewById(R.id.imageView4);
+
+
+        levi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/somanath08"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        astrix.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/aj-james"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        den.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://github.com/ashishraman96"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+        raiden.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://ujwalp15.github.io"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
     }
 }
