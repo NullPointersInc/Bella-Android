54556fb74adab6123964f610ed579267dfc37f8b
ujwalp15 <ujwalp09@gmail.com>
2017-04-01 21:57:20 +0530

    bella: re-do settings and get ready for release

--- app/src/main/java/com/example/android/Bella/SettingActivity.java
+++ app/src/main/java/com/example/android/Bella/SettingActivity.java
@@ -1,59 +1,165 @@
 package com.example.android.Bella;
 
-import android.content.Intent;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.transition.Fade;
-import android.view.View;
-import android.widget.TextView;
+import android.support.v4.content.ContextCompat;
+import com.danielstone.materialaboutlibrary.ConvenienceBuilder;
+import com.danielstone.materialaboutlibrary.MaterialAboutActivity;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutActionItem;
+import com.danielstone.materialaboutlibrary.items.MaterialAboutTitleItem;
+import com.danielstone.materialaboutlibrary.model.MaterialAboutCard;
+import com.danielstone.materialaboutlibrary.model.MaterialAboutList;
+import com.mikepenz.community_material_typeface_library.CommunityMaterial;
+import com.mikepenz.google_material_typeface_library.GoogleMaterial;
+import com.mikepenz.iconics.IconicsDrawable;
+import com.mikepenz.iconics.context.IconicsContextWrapper;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends MaterialAboutActivity {
 
-
-    TextView source;
-    TextView link;
-    Toolbar toolbar;
+    int colorIcon = R.color.colorIconDark;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        Fade s = new Fade();
-        s.setDuration(1000);
-        getWindow().setEnterTransition(s);
+    protected MaterialAboutList getMaterialAboutList(Context c) {
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.setting_activity);
+        MaterialAboutCard.Builder appCardBuilder = new MaterialAboutCard.Builder();
 
-        source = (TextView)findViewById(R.id.Source);
-        link = (TextView)findViewById(R.id.Link);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setNavigationIcon(R.drawable.ic_action_back);
-        toolbar.setTitleTextColor(0xFFFFFFFF);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                SettingActivity.super.onBackPressed();
-            }
-        });
+        appCardBuilder.addItem(new MaterialAboutTitleItem.Builder()
+                .text("Bella")
+                .icon(R.mipmap.bella_launcher)
+                .build());
 
-        source.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SettingActivity.this,WebActivity.class);
-                intent.putExtra("link", "https://github.com/ujwalp15/Bella");
-                startActivity(intent);
-            }
-        });
-        link.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SettingActivity.this,WebActivity.class);
-                intent.putExtra("link", "https://github.com/ujwalp15/Bella");
-                startActivity(intent);
-            }
-        });
+        try {
+
+            appCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(c,
+                    new IconicsDrawable(c)
+                            .icon(GoogleMaterial.Icon.gmd_info_outline)
+                            .color(ContextCompat.getColor(c, colorIcon))
+                            .sizeDp(18),
+                    "Version",
+                    false));
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        appCardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Changelog")
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_history)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickListener(ConvenienceBuilder.createWebViewDialogOnClickAction(c, "Releases", "https://github.com/ujwalp15/Bella/Releases", true, false))
+                .build());
+
+        MaterialAboutCard.Builder authorCardBuilder = new MaterialAboutCard.Builder();
+        authorCardBuilder.title("Author");
+//        authorCardBuilder.titleColor(ContextCompat.getColor(c, R.color.colorAccent));
+
+        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Null Pointers")
+                .subText("Bangalore")
+                .icon(new IconicsDrawable(c)
+                        .icon(GoogleMaterial.Icon.gmd_person)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .build());
+
+        authorCardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .text("Fork on GitHub")
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_github_circle)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .setOnClickListener(ConvenienceBuilder.createWebsiteOnClickAction(c, Uri.parse("https://github.com/ujwalp15/Bella")))
+                .build());
+
+        MaterialAboutCard.Builder convenienceCardBuilder = new MaterialAboutCard.Builder();
+
+        convenienceCardBuilder.title("Convenience Builder");
+        try {
+            convenienceCardBuilder.addItem(ConvenienceBuilder.createVersionActionItem(c,
+                    new IconicsDrawable(c)
+                            .icon(CommunityMaterial.Icon.cmd_information_outline)
+                            .color(ContextCompat.getColor(c, colorIcon))
+                            .sizeDp(18),
+                    "Revision",
+                    true));
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createWebsiteActionItem(c,
+                new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_earth)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18),
+                "Visit Website",
+                true,
+                Uri.parse("https://ujwalp15.github.io")));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createRateActionItem(c,
+                new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_star)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18),
+                "Rate this app",
+                null
+        ));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createEmailItem(c,
+                new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_email)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18),
+                "Send an email",
+                true,
+                "ujwalp09@gmail.com",
+                "Question concerning Bella"));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createPhoneItem(c,
+                new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_phone)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18),
+                "Call me",
+                true,
+                "9916637360"));
+
+        convenienceCardBuilder.addItem(ConvenienceBuilder.createMapItem(c,
+                new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_map)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18),
+                "Visit Bangalore",
+                null,
+                "bangalore"));
+
+        MaterialAboutCard.Builder otherCardBuilder = new MaterialAboutCard.Builder();
+        otherCardBuilder.title("Other");
+
+        otherCardBuilder.addItem(new MaterialAboutActionItem.Builder()
+                .icon(new IconicsDrawable(c)
+                        .icon(CommunityMaterial.Icon.cmd_language_html5)
+                        .color(ContextCompat.getColor(c, colorIcon))
+                        .sizeDp(18))
+                .text("Contributions")
+                .subTextHtml("We believe in <b>Open-Source</b> ideology. <br />Therefore, the <i>source</i> is readily available to work with. This gives other developers a chance to contribute to our project<br/>Remember that we accept only pull-requests")
+                .setIconGravity(MaterialAboutActionItem.GRAVITY_TOP)
+                .build()
+        );
+
+
+        return new MaterialAboutList(appCardBuilder.build(), authorCardBuilder.build(), convenienceCardBuilder.build(), otherCardBuilder.build());
+    }
+
+    @Override
+    protected CharSequence getActivityTitle() {
+        return getString(R.string.mal_title_about);
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+        super.attachBaseContext(IconicsContextWrapper.wrap(newBase));
     }
 }
