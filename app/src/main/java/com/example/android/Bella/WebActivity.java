0ce06b528de9fa9f31d7bb20683684e2455295c3
ashishraman96 <ashishraman81@gmail.com>
2017-04-02 16:04:28 +0530

    Add Progress Bar to WebView

--- app/src/main/java/com/example/android/Bella/WebActivity.java
+++ app/src/main/java/com/example/android/Bella/WebActivity.java
@@ -1,16 +1,24 @@
 package com.example.android.Bella;
 
+
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class WebActivity extends AppCompatActivity{
 
     String link;
     WebView w;
+    ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -19,10 +27,45 @@ public class WebActivity extends AppCompatActivity{
         setContentView(R.layout.webview_activity);
         link = getIntent().getStringExtra("link");
         Log.e("link:",link);
+        progressBar = (ProgressBar) findViewById(R.id.loading_spinner);
         w = (WebView) findViewById(R.id.link);
-
-        w.setWebViewClient(new WebViewClient());
-
+        w.setWebViewClient(new myWebClient());
         w.loadUrl(link);
+
+    }public class myWebClient extends WebViewClient
+    {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            // TODO Auto-generated method stub
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO Auto-generated method stub
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            // TODO Auto-generated method stub
+            super.onPageFinished(view, url);
+
+            progressBar.setVisibility(View.GONE);
+        }
+    }
+
+    // To handle "Back" key press event for WebView to go back to previous screen.
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event)
+    {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && w.canGoBack()) {
+            w.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
